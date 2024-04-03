890f9586b0013bc8521160cd8fb375e9a6e9a417
Dorian Rudolph <165898461+DorianR-FHDW@users.noreply.github.com>
2024-04-03 12:41:48 +0200

    Create Wetterstation.java

---
+++ Wetterstation.java
@@ -0,0 +1,25 @@
+public class Wetterstation {
+    public double temperatur;
+    public double windgeschwindigkeit;
+
+    public void setTemperatur(double temperatur) {
+        this.temperatur = temperatur;
+    }
+
+    public double getTemperatur() {
+        return this.temperatur;
+    }
+
+    public void setWindgeschwindigkeit(double windgeschwindigkeit) {
+        this.windgeschwindigkeit = windgeschwindigkeit;
+    }
+
+    public double getWindgeschwindigkeit() {
+        return this.windgeschwindigkeit;
+    }
+
+    public void Wetterkonditionen() {
+        System.out.println("aktuelle Temperatur: " + getTemperatur() + " °C");
+        System.out.println("aktuelle Windgeschwindigkeit: " + getWindgeschwindigkeit() + " km/h");
+    }
+}
